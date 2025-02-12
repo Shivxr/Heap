25cae95e23f06e4d09796f95da6c7baf03508d88
Shiv_Shankar <165561744+Shivxr@users.noreply.github.com>
2025-02-12 05:37:31 +0530

    Create heap.py

---
+++ heap.py
@@ -0,0 +1,57 @@
+def parent(ind):
+    if (ind-1)//2>=0:
+        return (ind-1)//2
+    return -1
+
+def left(ind,ln):
+    if (ind*2)+1<ln:
+        return (ind*2)+1
+    return -1
+
+def right(ind,ln):
+    if (ind*2)+2<ln:
+        return (ind*2)+2
+    return -1
+
+def smol(ind,hp,ln):
+    smol=-1
+    l,r=left(ind,ln),right(ind,ln)
+    if l!=-1 and r!=1:
+        if hp[l]<hp[r]:
+            smol=l
+        else:
+            smol=r
+    elif (l*r)<0:
+        if l!=-1:
+            smol=l
+        else:
+            smol=r
+    return smol
+        
+    
+
+def heapify(arr):
+    hp=[]
+    for i in range(len(arr)):
+        hp.append(arr[i])
+        while parent(i)!=-1 and hp[parent(i)]>hp[i]:
+            hp[i],hp[parent(i)]=hp[parent(i)],hp[i]
+            i=parent(i)
+    return hp
+
+def hins(a,hp):
+    hp.append(a)
+    i=len(hp)-1
+    while parent(i)!=-1 and hp[parent(i)]>hp[i]:
+            hp[i],hp[parent(i)]=hp[parent(i)],hp[i]
+            i=parent(i)
+            
+def hdel(ind,hp):
+    hp[ind],hp[-1]=hp[-1],hp[ind]
+    hp.pop()
+    x=smol(ind,hp,len(hp))
+    while x!=-1 and hp[ind]>hp[x]:
+        z=smol(ind,hp,len(hp))
+        hp[ind],hp[x]=hp[x],hp[ind]
+        ind=z
+        x=smol(ind,hp,len(hp))
